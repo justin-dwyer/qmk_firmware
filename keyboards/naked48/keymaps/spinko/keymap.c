50551522c8784fa933ee49cd07259de50f5e3bc8
Justin Dwyer <justindwyer6@gmail.com>
2020-09-28 18:34:18 -0700

    trying out no enter in base layer

--- keyboards/naked48/keymaps/spinko/keymap.c
+++ keyboards/naked48/keymaps/spinko/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    LT(NAV, KC_DEL),      KC_Q,               KC_W,             KC_E,                     KC_R,            KC_T,                                                       KC_Y,              KC_U,             KC_I,                       KC_O,             KC_P,                       KC_BSPC,                       \
    LT(OPPTEMP, KC_TAB),  KC_A,               KC_S,             KC_D,                     KC_F,            KC_G,                                                       KC_H,              KC_J,             KC_K,                       KC_L,             LT(NAV, KC_SCLN),           LT(OPPTEMP, KC_TAB),           \
    LT(SYM, KC_ESC),      LT(NPADTMP, KC_Z),  KC_X,             KC_C,                     KC_V,            KC_B,                                                       KC_N,              KC_M,             KC_COMM,                    RALT_T(KC_DOT),   LT(NPADTMP, KC_SLSH),       LT(SYM, KC_QUOT),              \
-                         LCTL_T(KC_GRV),     LALT_T(KC_EQL),   LALT_T(KC_MINS),          KC_LSFT,         LCMD_T(KC_SPC),  LT(OPPTEMP, KC_ENT),  LT(OPPTEMP,KC_ENT),  RCMD_T(KC_SPC),    KC_RSFT,          RALT_T(KC_LBRC),            RALT_T(KC_RBRC),  RCTL_T(KC_BSLS)                                            \
+                         LCTL_T(KC_GRV),     LALT_T(KC_EQL),   LALT_T(KC_MINS),          KC_LSFT,         LCMD_T(KC_SPC),  OPPTEMP,  OPPTEMP,  RCMD_T(KC_SPC),    KC_RSFT,          RALT_T(KC_LBRC),            RALT_T(KC_RBRC),  RCTL_T(KC_BSLS)                                            \
 ),
 
 [_NAV] = LAYOUT(
